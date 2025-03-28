3d315ec03295709cb01100be939103bccce19191
yeaminsakib <yeaminsakib@gmail.com>
2025-03-29 01:26:16 +0600

    pem file second

---
+++ Cryptography_assignment/56899.py
@@ -0,0 +1,7 @@
+from Crypto.PublicKey import RSA
+# Generate an RSA key
+key = RSA.generate(2048)
+# Create and write the PEM file
+with open('56899.pem', 'wb') as file: 
+    file.write(key.export_key('PEM'))
+
